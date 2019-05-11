a9b71445c5f4bedc98335943b3505eb92f7477ab
ihsuanlin1997 <50326223+ihsuanlin1997@users.noreply.github.com>
2019-05-11 16:35:25 +0800

    Update action.js

--- js/action.js
+++ js/action.js
@@ -6,22 +6,14 @@ var score = 10000;
 
 function highscore() {
   score=score+200;
-<<<<<<< HEAD
-  score.innerHTML="(" + score + ")";
-=======
-  document.getElementById("score").innerHTML="score";
+  document.getElementById("score").innerHTML=score;
 //   score.innerHTML="(" + score + ")";
->>>>>>> 386ec0d15ee5a269716963b344339c17bc81f6eb
 }
 
 function lowscore() {
   score=score-2000;
-<<<<<<< HEAD
-  score.innerHTML="(" + score + ")";
-=======
-  document.getElementById("score").innerHTML="score";
+  document.getElementById("score").innerHTML=score;
 //   score.innerHTML="(" + score + ")";
->>>>>>> 386ec0d15ee5a269716963b344339c17bc81f6eb
 }
 
 function low() {
@@ -137,11 +129,6 @@ function again(){
     document.getElementById("end").style.display = "none";
     document.getElementById("again").style.display = "none";
     score=10000;
-<<<<<<< HEAD
-    score.innerHTML="(" + score + ")";
-}
-=======
-    document.getElementById("score").innerHTML="score";
+    document.getElementById("score").innerHTML=score;
 //     score.innerHTML="(" + score + ")";
 }
->>>>>>> 386ec0d15ee5a269716963b344339c17bc81f6eb
